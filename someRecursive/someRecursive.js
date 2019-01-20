16dd2350de0f693eac895d752265a329dc2adf09
Yash Rajgor <yash592@gmail.com>
2019-01-20 11:40:32 -0800

    someRecursive

---
+++ someRecursive/someRecursive.js
@@ -0,0 +1,16 @@
+const isOdd = val => val % 2 !== 0;
+
+console.log(someRecursive([1,2,3,4], isOdd)) // true
+console.log(someRecursive([4,6,8,9], isOdd)) // true
+console.log(someRecursive([4,6,8], isOdd)) // false
+
+
+function someRecursive(arr, cb) {
+  if(arr.length === 0) return false;
+  if(cb(arr[0])) return true;
+  return someRecursive(arr.slice(1), cb)
+}
+
+// SAMPLE INPUT / OUTPUT
+
+// someRecursive([4,6,8], val => val > 10); // false
