a0f65488f2f173200ac1d4a7220fa0c2792eefbc
Yash Rajgor <yash592@gmail.com>
2019-01-24 17:59:48 -0800

    bst

--- BST/bst.js
+++ BST/bst.js
@@ -1,7 +1,5 @@
 // Binary Search Tree implementation
 
-// Binary Search Tree implementation
-
 class Node {
   constructor(val) {
     this.val = val;
@@ -46,6 +44,66 @@ class BinarySearchTree {
     }
   }
 
+//   find function
+  find(num) {
+    if(!this.root) return null;
+    else if(num === this.root.val) {
+      console.log('found it');
+      return this.root.val
+    }
+
+    let numFound = false;
+    let current = this.root;
+
+
+    while(!numFound && current) {
+      if(num > current.val) {
+        current = current.right;
+      } else if (num < current.val) {
+        current = current.left;
+      } else {
+        numFound = true;
+      }
+    }
+    return current;
+  }
+
+//   breadth first search
+
+  bfs() {
+    const data = []
+          queue = [];
+          node = this.root
+    queue.push(node)
+
+    while(queue.length) {
+      node = queue.shift()
+      data.push(node.val)
+      if(node.left) queue.push(node.left)
+      if(node.right) queue.push(node.right)
+    }
+    return data;
+
+  }
+
+
 }
 
 var tree = new BinarySearchTree();
+//          10
+//       6      15
+//     3  8       20
+
+
+tree.insert(10)
+tree.insert(6)
+tree.insert(14)
+tree.insert(3)
+tree.insert(7)
+tree.insert(11)
+tree.insert(15)
+tree.insert(1)
+tree.insert(4)
+tree.insert(8)
+tree.insert(9)
+tree.insert(12)
