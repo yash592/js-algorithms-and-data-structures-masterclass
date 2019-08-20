cd13197d2cfc2c13a4c6c6f2ff61b991ec83a7ec
Yash Rajgor <yash592@gmail.com>
2019-08-19 22:50:51 -0700

    reverseArray
    Works only for odd length array lol

---
+++ reverseArray/reverseArray.js
@@ -0,0 +1,21 @@
+const reverseArray = arr => {
+  let mid = Math.floor(arr.length / 2);
+  console.log(mid);
+  let s = 0;
+  let e = arr.length - 1;
+  console.log(s, e);
+
+  while (s < mid && e > mid) {
+    swap(arr, s, e);
+    s++;
+    e--;
+    console.log(s, e);
+  }
+  return arr;
+};
+
+const swap = (array, idx1, idx2) => {
+  [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+};
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
