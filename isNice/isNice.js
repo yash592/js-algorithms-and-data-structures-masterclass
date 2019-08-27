3a683f29b1004a2afe1f302415f5e8244f0a2d05
Yash Rajgor <yash592@gmail.com>
2019-08-26 22:56:08 -0700

    isniceOptimizedPseudocode

--- isNice/isNice.js
+++ isNice/isNice.js
@@ -12,4 +12,10 @@ const isNice = arr => {
   else return false;
 };
 
+const isNiceOptimized = arr => {
+  // loop only once over the array
+  // if 2-3 = n-d then the reverse will be true as well i.e 3-2 = 1
+  //
+};
+
 isNice([2, 10, 9, 3]);
